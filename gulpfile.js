919b9e3406b26e99ad112509a26fcc39c7cf9497
Aswin Unnikrishna pillai <Aswin Unnikrishna pillai>
2016-05-26 11:53:01 +0100

    Added reporters to jasmiine

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const gulpWebserver = require('gulp-webserver');
 const jasmine = require('gulp-jasmine');
+const reporters = require('jasmine-reporters');
 
 gulp.task('lint', function(){
     return gulp.src(['**/*.js', '!node_modules/**'])
@@ -11,11 +12,17 @@ gulp.task('lint', function(){
 
 });
 
-gulp.task('specs', function(done){
+gulp.task('specs', function (done) {
     gulp.src('spec/*.js')
-        .pipe(jasmine());
+        .pipe(jasmine({
+            reporter: new reporters.TerminalReporter({
+                verbosity: 3,
+                color: true
+            })
+        }));
     done()
 })
+
 gulp.task('run', function(done){
     gulp.src('src')
         .pipe(gulpWebserver({
