4e329094060e1355e7a31af032b01aabbb9cbf5b
Aswin Unnikrishna pillai <Aswin Unnikrishna pillai>
2016-05-26 10:14:02 +0100

    initialised gulp

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ This is my project
 ## Installation
  type: npm install
 ## Usage
+To build:
+1.gulp
 ## credits
 ## License
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const gulp = require('gulp');
+gulp.task('default', function(done){
+    console.log('Build ok');
+    done();
+});
