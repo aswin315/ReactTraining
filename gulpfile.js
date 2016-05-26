c28433ebc0cb03c4a3a1b9ff2b5dd6c2aa54410b
Aswin Unnikrishna pillai <Aswin Unnikrishna pillai>
2016-05-26 11:25:27 +0100

    installed jasmine

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const gulpWebserver = require('gulp-webserver');
+
 gulp.task('lint', function(){
     return gulp.src(['**/*.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 
+});
+
+gulp.task('run', function(done){
+    gulp.src('src')
+        .pipe(gulpWebserver({
+            livereload: true,
+            open: true
+        }));
 });
 
 gulp.task('default', gulp.series('lint', function (done) {
