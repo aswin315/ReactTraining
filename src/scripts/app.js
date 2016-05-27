1e4ec623d1cfc8b5e813b147cd4a7acefd5da120
Aswin Unnikrishna pillai <Aswin Unnikrishna pillai>
2016-05-27 08:31:16 +0100

    introduced first react component

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import sayHello from './sayHello.js';
+
+var SayHello = React.createClass({
+    render: function(){
+        return <h1>
+            Hello, {this.props.name}
+            </h1>;
+    }
+});
+
+ReactDOM.render(
+    <SayHello name='world'/>,
+    document.getElementById('app')
+);
