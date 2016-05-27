5404e7c20cb5f3b635dadbffed8f77c4bec49c9e
Aswin Unnikrishna pillai <Aswin Unnikrishna pillai>
2016-05-27 10:49:42 +0100

    React component for displaying questions

---
+++ src/scripts/SubmitButton.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+ 
+ class Button extends React.Component{
+     render(){
+         return(
+                 <div>
+             <input type='submit'/>Go!
+             </div>
+         );
+     }
+ }
+ export default Button;
